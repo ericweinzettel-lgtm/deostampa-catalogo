d95e39bb94eb48a2e9556d33f26c16336eef7220
ericweinzettel-lgtm <ericweinzettel@outlook.it>
2026-01-18 19:28:25 +0100

    Update types.ts

--- types.ts
+++ types.ts
@@ -6,7 +6,7 @@ export interface Product {
   price: number;
   category: string;
   image: string;
-  images: string[];
+  images?: string[];
   isCustomizable: boolean;
   ebayUrl: string; // Link diretto all'inserzione eBay
 }
